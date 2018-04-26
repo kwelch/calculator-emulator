91f46ed5134517fa8da5f43ecb0c67593533d9ba
Kyle Welch <kwelch0626@gmail.com>
2018-04-25 19:03:24 -0500

    added base look and feel of calculator

--- src/App.js
+++ src/App.js
@@ -1,18 +1,103 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    displayValue: '',
+  };
+
+  updateDisplay = evt => {
+    let value = evt.target.innerText
+    this.setState(prevState => ({
+      displayValue: `${prevState.displayValue}${value}`,
+    }));
+  };
+
+  handleInputChange = evt => {
+    let value = evt.target.value;
+    this.setState({
+      displayValue: value,
+    });
+  };
+
+  runCalculation = () => {
+    
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form>
+          <table>
+            <thead>
+              <tr>
+                <td colSpan="4">
+                  <input
+                    className="calculator__display"
+                    onChange={this.handleInputChange}
+                    value={this.state.displayValue}
+                  />
+                </td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td colSpan="3">Clear</td>
+                <td>%</td>
+              </tr>
+              <tr>
+                <td>
+                  <span onClick={this.updateDisplay}>7</span>
+                </td>
+                <td>
+                  <span onClick={this.updateDisplay}>8</span>
+                </td>
+                <td>
+                  <span onClick={this.updateDisplay}>9</span>
+                </td>
+                <td>
+                  <span onClick={this.updateDisplay}>x</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span onClick={this.updateDisplay}>4</span>
+                </td>
+                <td>
+                  <span onClick={this.updateDisplay}>5</span>
+                </td>
+                <td>
+                  <span onClick={this.updateDisplay}>6</span>
+                </td>
+                <td>
+                  <span onClick={this.updateDisplay}>-</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span onClick={this.updateDisplay}>1</span>
+                </td>
+                <td>
+                  <span onClick={this.updateDisplay}>2</span>
+                </td>
+                <td>
+                  <span onClick={this.updateDisplay}>3</span>
+                </td>
+                <td>
+                  <span onClick={this.updateDisplay}>+</span>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="3">
+                  <span onClick={this.updateDisplay}>0</span>
+                </td>
+                <td>
+                  <span onClick={this.runCalculation}>=</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </form>
       </div>
     );
   }
